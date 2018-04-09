d11cce65dd6e8e5055757ec5d1298e41f1823c90
Luis Rollón Gordo <luisrollongordo@gmail.com>
2018-04-09 11:27:10 +0200

    Eliminados comentarios por defecto y añadido mensaje por consola.

--- GitExample/ViewController.swift
+++ GitExample/ViewController.swift
@@ -12,14 +12,14 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("Este es un proyecto de ejemplo para ver el funcionamiento de un repositorio git")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
-
 
 }
 
